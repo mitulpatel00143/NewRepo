7bc7a5191bf574fc25105b40354407ce877b4560
Mitul <mitulpatel00143@gmail.com>
2023-04-13 15:01:16 +0530

    file added

---
+++ BowlingGame.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bowling_Game
+{
+    public class BowlingGame
+    {
+        private int[] rolls = new int[21];
+        private int currentRoll = 0;
+
+        public void Roll(int pins)
+        {
+            rolls[currentRoll++] = pins;
+        }
+
+        public int Score()
+        {
+            int score = 0;
+            int rollIndex = 0;
+
+            for (int frame = 0; frame < 10; frame++)
+            {
+                if (IsStrike(rollIndex))
+                {
+                    score += 10 + StrikeBonus(rollIndex);
+                    rollIndex++;
+                }
+                else if (IsSpare(rollIndex))
+                {
+                    score += 10 + SpareBonus(rollIndex);
+                    rollIndex += 2;
+                }
+                else
+                {
+                    score += SumOfBallsInFrame(rollIndex);
+                    rollIndex += 2;
+                }
+            }
+
+            return score;
+        }
+
+        private bool IsStrike(int rollIndex)
+        {
+            return rolls[rollIndex] == 10;
+        }
+
+        private bool IsSpare(int rollIndex)
+        {
+            return rolls[rollIndex] + rolls[rollIndex + 1] == 10;
+        }
+
+        private int StrikeBonus(int rollIndex)
+        {
+            return rolls[rollIndex + 1] + rolls[rollIndex + 2];
+        }
+
+        private int SpareBonus(int rollIndex)
+        {
+            return rolls[rollIndex + 2];
+        }
+
+        private int SumOfBallsInFrame(int rollIndex)
+        {
+            return rolls[rollIndex] + rolls[rollIndex + 1];
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Bowling_Game;
+
+namespace Bowling_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bowlingGame = new BowlingGame();
+
+            bowlingGame.Roll(4);
+            bowlingGame.Roll(4);
+            Console.WriteLine("Score: " + bowlingGame.Score());
+
+            var bowlingGame1 = new BowlingGame();
+            bowlingGame1.Roll(4);
+            bowlingGame1.Roll(6);
+            bowlingGame1.Roll(5);
+            bowlingGame1.Roll(0);
+            Console.WriteLine("Score: " + bowlingGame1.Score());
+
+            var bowlingGame2 = new BowlingGame();
+            bowlingGame2.Roll(10);
+            bowlingGame2.Roll(5);
+            bowlingGame2.Roll(4);
+            Console.WriteLine("Score: " + bowlingGame2.Score());
+
+            var bowlingGame3 = new BowlingGame();
+            bowlingGame3.Roll(10);
+            bowlingGame3.Roll(10);
+            bowlingGame3.Roll(10);
+            Console.WriteLine("Score: " + bowlingGame3.Score());
+
+            Console.ReadLine();
+        }
+    }
+}
